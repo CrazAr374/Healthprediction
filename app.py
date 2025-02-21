9177b1360548c43e7ae3cff8005de97c706db86f
Atharva374 <133023029+CrazAr374@users.noreply.github.com>
2025-02-21 18:27:18 +0530

    Update app.py

--- app.py
+++ app.py
@@ -232,5 +232,6 @@ def get_recommendations(disease):
 
     return formatted_recs
 
-if __name__ == '__main__':
-    app.run(debug=True)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=10000, debug=True)  # Change port if needed
+
